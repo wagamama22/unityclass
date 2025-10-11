951611b192aaa4eac1e5ae36a6787e1beffe6f36
wagamama22 <wagajob@gmail.com>
2025-10-11 05:14:39 +0100

    updated the cube

--- laserDefender/Assets/scripts/box.cs
+++ laserDefender/Assets/scripts/box.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 
-public class rotateCube : MonoBehaviour
+public class box : MonoBehaviour
 {
+    public float rotationSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rotationSpeed = 80f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
     }
 }
