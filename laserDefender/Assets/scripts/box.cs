dcb96eeb729e611006f294b61bac3d6fe44ec941
wagamama22 <wagajob@gmail.com>
2025-10-13 10:43:28 +0100

    update on rotaing cube

--- laserDefender/Assets/scripts/box.cs
+++ laserDefender/Assets/scripts/box.cs
@@ -1,17 +1,38 @@
 using UnityEngine;
 
-public class box : MonoBehaviour
+
+public class Box : MonoBehaviour
 {
-    public float rotationSpeed;
+    // Rotation speed in degrees per second
+    // Adjust this value to change how fast the cube rotates
+    [SerializeField] private float rotationSpeed = 80f;
+    [SerializeField] private float speedIncrement = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rotationSpeed = 80f;
+        Debug.Log("Cube is rotating");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Check for up arrow keypress to increase rotation speed
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rotationSpeed += speedIncrement;
+            Debug.Log("Current rotation speed: " + rotationSpeed);
+        }
+
+        // Check for down arrow keypress to decrease rotation speed
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rotationSpeed -= speedIncrement;
+            Debug.Log("Current rotation speed: " + rotationSpeed);
+        }
+
+        // Rotate the object around the y-axis (up vector) at the specified speed
+        // Multiply by Time.deltaTime to make the rotation frame-rate independent
         transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
     }
 }
