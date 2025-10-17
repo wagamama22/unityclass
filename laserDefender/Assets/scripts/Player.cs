1ed95f080271a0b1ecaa7433f4625012d3e32e66
wagamama22 <wagajob@gmail.com>
2025-10-17 01:20:32 +0100

    addscreen wrapping boundaries

--- laserDefender/Assets/scripts/Player.cs
+++ laserDefender/Assets/scripts/Player.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 5f; // Movement speed
+    private Vector2 movement;
+    [Header("Screen Wrap Bounds")]
+    [SerializeField] private float minX = -4.43f;
+    [SerializeField] private float maxX = 4.32f;
+    [SerializeField] private float minY = -8.47f;
+    [SerializeField] private float maxY = 8.52f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,11 +23,35 @@ public class Player : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal"); // Left/Right arrows
         float moveY = Input.GetAxis("Vertical");   // Up/Down arrows
 
+
         // Create movement vector
-        Vector2 movement = new Vector2(moveX, moveY)* speed * Time.deltaTime;
+        Vector2 movement = new Vector2(moveX, moveY) * speed * Time.deltaTime;
 
         // Apply the movement to the player 's transform'
-        transform.Translate(movement);
+        transform.Translate(movement);//transform has to do with position of gameobject while translate renders the transform action.
+
+        //screen wrapping for bounds
+        Vector3 newPosition = transform.position;
+
+        //boundaries for x-axis
+        if (newPosition.x > maxX)//newPosition.x and .y means the current position of the player in the screen
+        {
+            newPosition.x = maxX;
+        }
+        else if (newPosition.x < minX) 
+        {
+            newPosition.x = minX;
+        }
+        //boundaries for y-axis
+        if (newPosition.y > maxY) 
+        {
+            newPosition.y = maxY;
+        }
+        else if (newPosition.y < minY) 
+        {
+            newPosition.y = minY;
+        }
+        transform .position = newPosition;//sets the position to newPosition
     }
 
     // Update is called once per frame
